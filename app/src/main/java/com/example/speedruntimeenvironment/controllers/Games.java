3941b6c255a1e68713046f29654f72090b07f8d8
nils <nilsjakubeit@web.de>
2018-07-01 20:18:42 +0200

    - beliebtesten Games nun aus JSON-File - Games-Uebersicht nun korrekt aufgebaut

--- app/src/main/java/com/example/speedruntimeenvironment/controllers/Games.java
+++ app/src/main/java/com/example/speedruntimeenvironment/controllers/Games.java
@@ -17,11 +17,9 @@ import com.example.speedruntimeenvironment.daos.impl.GamesDAOImpl;
 import com.example.speedruntimeenvironment.model.GameList;
 
 import org.json.JSONException;
-import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 
@@ -29,12 +27,7 @@ public class Games extends Fragment {
 
     private static final String TAG = "Games";
 
-    private final String GAMES_FILE = "popular_games.json";   // TODO
-
-    // listen fuer recyclerview
-    private List<String> mGameNames = new ArrayList<>();
-    private List<String> mGameImageUrls = new ArrayList<>();
-    private List<String> mGameIds = new ArrayList<>();
+    private final String GAMES_FILE = "popular_games.json";   //
 
     // neu: eine zentrale Liste
     private GameList mGameList = new GameList();    // muss nicht init werden hier TODO: check
@@ -85,18 +78,6 @@ public class Games extends Fragment {
             Log.e(TAG, "initGames: Error while parsing JSON-file", e);  //TODO: Toast hier: "Couldn't load games"
         }
 
-
-        /*
-        mGameImageUrls.add("https://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_dx_18-140mmf_35-56g_ed_vr/img/sample/img_01.jpg");
-        mGameNames.add("Spatz");
-
-        mGameImageUrls.add("https://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_dx_18-140mmf_35-56g_ed_vr/img/sample/img_02.jpg");
-        mGameNames.add("Kirche");
-
-        mGameImageUrls.add("https://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_dx_18-140mmf_35-56g_ed_vr/img/sample/img_03.jpg");
-        mGameNames.add("Paris");
-        */
-
         initRecyclerView(v);
 
     }
@@ -111,44 +92,7 @@ public class Games extends Fragment {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
 
-        // updateList();
-
-        // erstelleGamesListe();
-
-
-
     }
-
-    private void updateList() {
-        Log.d(TAG, "updateList: START");
-
-        List<String> neueImageUrls = new ArrayList<>();
-        List<String> neueImageNames = new ArrayList<>();
-
-        neueImageUrls.add("https://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_dx_18-140mmf_35-56g_ed_vr/img/sample/img_04.jpg");
-        neueImageNames.add("Blumenfeld");
-
-        neueImageUrls.add("https://imgsv.imaging.nikon.com/lineup/lens/zoom/normalzoom/af-s_dx_18-140mmf_35-56g_ed_vr/img/sample/img_05.jpg");
-        neueImageNames.add("Statue");
-
-        // mNames = neueImageNames;
-        // mImageUrls = neueImageUrls;
-
-        adapter.setImageNames(neueImageNames);
-        adapter.setImages(neueImageUrls);
-
-        adapter.notifyDataSetChanged();
-    }
-
-    private void erstelleGamesListe() {
-        Log.d(TAG, "erstelleGamesListe: START");
-
-        client.getGamesBulk(this.adapter);
-
-
-    }
-
-
 
 
 }
