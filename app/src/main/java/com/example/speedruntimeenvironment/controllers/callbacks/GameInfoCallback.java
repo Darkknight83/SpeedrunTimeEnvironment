cfdf6dccdbebef9a7352d18fe356591b3405b972
nils <nilsjakubeit@web.de>
2018-07-02 22:42:17 +0200

    nun callback um DTOs weiterzureichen 2/2

---
+++ app/src/main/java/com/example/speedruntimeenvironment/controllers/callbacks/GameInfoCallback.java
@@ -0,0 +1,10 @@
+package com.example.speedruntimeenvironment.controllers.callbacks;
+
+import com.example.speedruntimeenvironment.model.Game;
+
+public interface GameInfoCallback {
+
+    void onSuccess(Game game);
+
+    void onFail();
+}
